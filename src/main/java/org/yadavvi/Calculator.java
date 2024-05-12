1751bcf5c40af71fd53464ea62b54580e45582ef
Vishal Yadav <yadavvi91@gmail.com>
2024-05-12 15:46:06 +0530

    Extract parameter object

--- src/main/java/org/yadavvi/Calculator.java
+++ src/main/java/org/yadavvi/Calculator.java
@@ -3,18 +3,18 @@ package org.yadavvi;
 public class Calculator {
     public Money calculate(Integer weight, Integer height, Integer width, Integer depth, Currency currency)
             throws Exception {
-        var postageInBaseCurrency = PostageInBaseCurrency(weight, height, width, depth);
+        var postageInBaseCurrency = PostageInBaseCurrency(new Dimensions(weight, height, width, depth));
         return ConvertCurrency(postageInBaseCurrency, currency);
     }
 
-    private Double PostageInBaseCurrency(Integer weight, Integer height, Integer width, Integer depth) {
-        if (weight <= 60 && height <= 229 && width <= 162 && depth <= 25) {
+    private Double PostageInBaseCurrency(Dimensions dimensions) {
+        if (dimensions.weight() <= 60 && dimensions.height() <= 229 && dimensions.width() <= 162 && dimensions.depth() <= 25) {
             return 120d;
         }
-        if (weight <= 500 && height <= 324 && width <= 229 && depth <= 100) {
-            return (double) (weight * 4);
+        if (dimensions.weight() <= 500 && dimensions.height() <= 324 && dimensions.width() <= 229 && dimensions.depth() <= 100) {
+            return (double) (dimensions.weight() * 4);
         }
-        return Math.max(weight, height * width * depth / 1000d) * 6;
+        return Math.max(dimensions.weight(), dimensions.height() * dimensions.width() * dimensions.depth() / 1000d) * 6;
     }
 
     private Money ConvertCurrency(double amountInBaseCurrency, Currency currency) throws Exception {
@@ -22,5 +22,8 @@ public class Calculator {
         if (currency == Currency.Eur) return new Money(Currency.Eur, (amountInBaseCurrency + 200) * 1.25d);
         if (currency == Currency.Chf) return new Money(Currency.Chf, (amountInBaseCurrency + 200) * 1.36d);
         throw new Exception("Currency not supported");
+    }
+
+    private static record Dimensions(Integer weight, Integer height, Integer width, Integer depth) {
     }
 }
