58daa87d126345eecb8b3440558e9b44bbea696d
Vishal Yadav <yadavvi91@gmail.com>
2024-05-18 13:47:57 +0530

    As per ChatGPT, creation of Packages should be with Package sealed interface

--- src/main/java/org/yadavvi/CalculatorApp.java
+++ src/main/java/org/yadavvi/CalculatorApp.java
@@ -2,6 +2,26 @@ package org.yadavvi;
 
 sealed interface Package permits SmallPackage, MediumPackage, DefaultPackage {
     Double postageInBaseCurrency();
+
+    private static boolean isMediumPackage(Dimension dimension) {
+        return dimension.weight() <= 500 && dimension.height() <= 324
+                && dimension.width() <= 229 && dimension.depth() <= 100;
+    }
+
+    private static boolean isSmallPackage(Dimension dimension) {
+        return dimension.weight() <= 60 && dimension.height() <= 229
+                && dimension.width() <= 162 && dimension.depth() <= 25;
+    }
+
+    static Package createPackage(Dimension dimension) {
+        if (isSmallPackage(dimension)) {
+            return new SmallPackage(dimension);
+        } else if (isMediumPackage(dimension)) {
+            return new MediumPackage(dimension);
+        } else {
+            return new DefaultPackage(dimension);
+        }
+    }
 }
 
 record SmallPackage(Dimension dimension) implements Package {
@@ -31,31 +51,10 @@ record Dimension(Integer weight, Integer height, Integer width, Integer depth) {
 }
 
 public class CalculatorApp {
-
-    private static boolean isMediumPackage(Dimension dimension) {
-        return dimension.weight() <= 500 && dimension.height() <= 324
-                && dimension.width() <= 229 && dimension.depth() <= 100;
-    }
-
-    private static boolean isSmallPackage(Dimension dimension) {
-        return dimension.weight() <= 60 && dimension.height() <= 229
-                && dimension.width() <= 162 && dimension.depth() <= 25;
-    }
-
-    static Package createPackage(Dimension dimension) {
-        if (isSmallPackage(dimension)) {
-            return new SmallPackage(dimension);
-        } else if (isMediumPackage(dimension)) {
-            return new MediumPackage(dimension);
-        } else {
-            return new DefaultPackage(dimension);
-        }
-    }
-
     public Money calculate(Integer weight, Integer height, Integer width, Integer depth, Currency currency)
             throws Exception {
         Dimension dimension = new Dimension(weight, height, width, depth);
-        Package postage = CalculatorApp.createPackage(dimension);
+        Package postage = Package.createPackage(dimension);
         var postageInBaseCurrency = postage.postageInBaseCurrency();
         return currency.convertCurrency(postageInBaseCurrency);
     }
